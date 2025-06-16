8f3a06e862f577ad6fc29211463d4d264ad14ca1
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-06-16 17:14:44 +0300

    Refactor user registration and login to include email uniqueness check and improve JWT payload structure

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -21,12 +21,15 @@ export const register = async ({
   const findUser = await userModel.findOne({ username });
   if (findUser) return { data: "User Already exist", statusCode: 400 };
 
-  const newUser = new userModel({ username, email, password, role });
+  const findEmail = await userModel.findOne({ email });
+  if (findEmail) return { data: "Email already in use", statusCode: 400 };
+
+  const newUser = new userModel({ username, email, password, role: "user" });
 
   await newUser.save();
   return {
     statusCode: 201,
-    data: generateJWT({ username, email, password, role }),
+    data: generateJWT({ id: newUser._id, username, email, role }),
   };
 };
 
@@ -42,12 +45,20 @@ export const login = async ({ username, password }: LoginParams) => {
   const passwordMatch = await bcrypt.compare(password, findUser.password);
 
   if (passwordMatch)
-    return { data: generateJWT({ username, password }), statusCode: 200 };
+    return {
+      data: generateJWT({
+        id: findUser._id,
+        username: findUser.username,
+        role: findUser.role,
+      }),
+      statusCode: 200,
+    };
 
   //else
   return { data: "Incorrect password", statusCode: 400 };
 };
 
 function generateJWT(data: any) {
-  return jwt.sign(data, process.env.JWT_SECRET as string);
+  if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not defined");
+  return jwt.sign(data, process.env.JWT_SECRET as string, { expiresIn: "1d" });
 }

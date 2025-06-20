f25bd03ca67e729d4622a03b9ca64cab83654081
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-06-20 19:56:28 +0300

    Update Card component to use correct routing path for blog links

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import { useBlog } from "../context/BlogContext";
 import { useUser } from "../context/userContext";
 

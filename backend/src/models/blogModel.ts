372c87747824e2efec045690a9db826775032fbf
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-06-25 04:47:13 +0300

    Update blog model to define claps as an array of user IDs, enhancing type safety and consistency in the schema.

--- backend/src/models/blogModel.ts
+++ backend/src/models/blogModel.ts
@@ -6,7 +6,7 @@ interface IBlog extends Document {
   category: string;
   authorId: ObjectId | string;
   img?: string;
-  claps: Array<string>;
+  claps: Array<ObjectId | string>;
 }
 
 const blogSchema = new Schema<IBlog>(
@@ -16,7 +16,7 @@ const blogSchema = new Schema<IBlog>(
     category: { type: String },
     authorId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     img: { type: String },
-    claps: [{ type: String }],
+    claps: [{ type: Schema.Types.ObjectId }],
   },
   { timestamps: true }
 );

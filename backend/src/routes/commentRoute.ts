f077365f704bd2a01fb9ed8a2d9230fec35e3979
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-06-25 05:37:15 +0300

    Add getComments functionality to comment routes and service. Implemented a new endpoint to retrieve comments for a specific blog, enhancing comment management capabilities.

--- backend/src/routes/commentRoute.ts
+++ backend/src/routes/commentRoute.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { validateJWT } from "../middlewares/validateJWT";
 import { IExtendRequest } from "../types/extendedRequest";
-import { addComment } from "../services/commentService";
+import { addComment, getComments } from "../services/commentService";
 
 const router = express.Router();
 
@@ -12,8 +12,20 @@ router.post("/", validateJWT, async (req: IExtendRequest, res) => {
     if (!authorId || !blogId || !comment) {
       res.status(400).send("Missing required fields");
     }
-    const result = await addComment(authorId, blogId, comment);
-    res.status(result.statusCode).send(result.data);
+    const { data, statusCode } = await addComment(authorId, blogId, comment);
+    res.status(statusCode).send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+router.get("/", validateJWT, async (req: IExtendRequest, res) => {
+  try {
+    const { blogId } = req.body;
+    const { data, statusCode } = await getComments(blogId);
+
+    res.status(statusCode).json(data);
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal server error");
--- backend/src/services/commentService.ts
+++ backend/src/services/commentService.ts
@@ -1,3 +1,5 @@
+import { ObjectId } from "mongoose";
+import blogModel from "../models/blogModel";
 import commentModel from "../models/commentModel";
 
 export const addComment = async (
@@ -8,6 +10,16 @@ export const addComment = async (
   try {
     const newComment = await commentModel.create({ authorId, blogId, comment });
     return { data: newComment, statusCode: 201 };
+  } catch (error) {
+    console.error(error);
+    return { data: "Something went wrong", statusCode: 500 };
+  }
+};
+
+export const getComments = async (blogId: ObjectId) => {
+  try {
+    const comments = await commentModel.find({ blogId: blogId }).lean();
+    return { data: comments, statusCode: 200 };
   } catch (error) {
     console.error(error);
     return { data: "Something went wrong", statusCode: 500 };
